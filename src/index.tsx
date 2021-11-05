7c9ab224d8400f5fe1820f39b04526c4a73b857d
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2021-11-05 12:38:22 +0500

    feat: gh-page deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import './styles/index.css'
 import './assets/fonts/Roboto-Medium.ttf'
 import './assets/fonts/Roboto-Regular.ttf'
